9c22fd6ddd8566f9bea1c05fb3d3daef5cdf6868
TEJA KESARAPU <145187633+TEJAKESARAPU@users.noreply.github.com>
2024-07-03 20:58:12 +0530

    added js_basics.html

---
+++ js_basics.js
@@ -0,0 +1,38 @@
+// Primitive / Value Types 
+// String, Number, Boolean, Undefined, Null, Symbol
+//JS - Dynamic language
+// Reference types - Objects, Arrays, Functions
+
+// Arrays and Functions are objects
+ 
+// Execution Context
+// 1. Memory Phase - Variable Environment
+// 2. Code Phase - Thread of execution
+
+// Hoisting
+// Temporal Dead Zone
+
+// const, let, var
+// const, let - Block Scoped
+
+// Lexical  Scope
+
+// Functions - First class citizens
+// Higher-order Function
+// Function + Lexical Scope = Closure
+
+// callback
+// event queue
+// event loop
+
+// Problems - Callback hell, Pyramis of Doom
+
+// Promises - object
+
+// 3 states - pending, Fullfilled, Rejected
+
+//Common Higher Order functions
+
+// 1. Map
+// 2. Filter
+// 3. Reduce
